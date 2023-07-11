7d27cb84ff210e588276061a9e724ed1197c4e0f
junho <qkrwnsgh1288@daum.net>
2023-07-11 18:27:23 +0900

    init

--- src/class/ExtendedClient.js
+++ src/class/ExtendedClient.js
@@ -39,7 +39,7 @@ module.exports = class extends Client {
 
         commands(this);
         events(this);
-        mongoose();
+        // mongoose();
 
         if (config.handler.deploy) deploy(this, config);
     };
